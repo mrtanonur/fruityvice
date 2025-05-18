22197ca2234f791bd5feb7b0b94d9c7bdccd84c7
mrtanonur <onurtan@gmail.com>
2025-05-18 14:37:17 +0300

    feat(core): inject provider and add theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:fruityvice/pages/main_page.dart';
+import 'package:fruityvice/providers/settings_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:fruityvice/providers/fruit_provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    MultiProvider(providers: [
+      ChangeNotifierProvider(create: (context) => FruitProvider()),
+      ChangeNotifierProvider(create: (context) => SettingsProvider())
+    ], child: const MainApp()),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +18,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        theme: context.watch<SettingsProvider>().isDark
+            ? ThemeData.dark()
+            : ThemeData.light(),
+        debugShowCheckedModeBanner: false,
+        home: const MainPage());
   }
 }
